eb779391594c2c6c3ef6fba0b195c5279a875876
chrispiech <piech@cs.stanford.edu>
2019-06-24 21:27:57 -0500

    auto commit

--- docs/starter/Day1es/src/FilaDeConos.java
+++ docs/starter/Day1es/src/FilaDeConos.java
@@ -1,16 +1,25 @@
-
+/**
+ * Programa : FilaDeConos.
+ * -----------------------
+ * Llene la primera fila con conos. Supone que
+ * Karel comienza en la esquina inferior izquierda.
+ * De la pantalla, mirando hacia el este.
+ */
 public class FilaDeConos extends EsKarel {
 	
-	/**
-	 * 
-	 */
-	
-	public void run() {
-		while(frenteDespejado()) {
+	public void run () {
+		while (frenteDespejado()) {
 			ponerCono();
 			moverse();
 		}
+		
+		/* Esta línea es necesaria para colocar el 
+		 * cono final. El numero de veces que Karel 
+		 * se mueve es menos que el numero de veces
+		 * Karel pone un cono (si el mundo tiene 
+		 * cinco cuadrados de ancho, ponemos 5 conos 
+		 * pero solo se mueven 4 veces)
+		 */
 		ponerCono();
 	}
-
-}
+} 

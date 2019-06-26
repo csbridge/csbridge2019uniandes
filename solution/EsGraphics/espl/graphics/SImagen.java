400084059c61ae14130599b34c5f50e43fb4571d
nathandalal <nathanhdalal@gmail.com>
2019-06-26 09:42:00 -0500

    get rid of commit messages in docs lol

--- solution/EsGraphics/espl/graphics/SImagen.java
+++ solution/EsGraphics/espl/graphics/SImagen.java
@@ -5,13 +5,41 @@ import acm.graphics.GImage;
 
 public class SImagen extends GImage implements SObjeto{
 
-	public SImagen(String name) {
-		super(name);
+	public SImagen(String nombreDeArchivo) {
+		super(nombreDeArchivo);
 	}
 
-	public SImagen(String name, double x, double y) {
-		super(name, x, y);
+	public SImagen(String nombreDeArchivo, double x, double y) {
+		super(nombreDeArchivo, x, y);
 	}
+
+	public SImagen(int[][] arregloDePixeles) {
+		super(arregloDePixeles);
+	}
+
+	public SImagen(int[][] arregloDePixeles, double x, double y) {
+		super(arregloDePixeles);
+	}
+
+	public void guardarImagen(String nombreDeArchivo) {
+		saveImage(nombreDeArchivo);
+	}
+
+	public int[][] darArregloDePixeles() {
+		return getPixelArray();
+	}
+
+	/* Utilidades de pixeles. */
+	public int crearRGBPixel(int rojo, int verde, int azul) {
+		return createRGBPixel(rojo, verde, azul)
+	}
+	public int crearRGBPixel(int rojo, int verde, int azul, int alpha) {
+		return createRGBPixel(rojo, verde, azul, alpha)
+	}
+	public int darAlpha(int pixel) { return getAlpha(pixel); }
+	public int darRojo(int pixel) { return getRed(pixel); }
+	public int darVerde(int pixel) { return getGreen(pixel); }
+	public int darAzul(int pixel) { return getBlue(pixel); }
 	
 
 	public void moverse(double dx, double dy) {

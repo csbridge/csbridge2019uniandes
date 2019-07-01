9222b2b4a844112c6d17177d49b3dc5928dd58d5
chrispiech <piech@cs.stanford.edu>
2019-07-01 09:13:57 -0500

    auto commit

--- docs/starter/Day5es/src/ReporteroMouse.java
+++ docs/starter/Day5es/src/ReporteroMouse.java
@@ -7,9 +7,13 @@ public class ReporteroMouse extends EsGraphics {
 
 	public void run() {
 		// tu codigo aca
+		agregarMouseListeners();
 	}
 	
 	public void mouseMovido(MouseEvento e) {
 		// tu codigo aca
+		int x = e.darX();
+		int y = e.darY();
+		imprimir("x: " + x + ", y:" + y);
 	}
 }
